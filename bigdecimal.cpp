3024ccdb43dd8bd31aafbff4512c2b504b4e45d4
ahmedyasserbastawessy <101553898+ahmedyasserbastawessy@users.noreply.github.com>
2022-10-12 23:22:51 +0200

    Update bigdecimal.cpp

--- bigdecimal.cpp
+++ bigdecimal.cpp
@@ -10,5 +10,58 @@ void reverseStr(string& str)
     for (int i = 0; i < n / 2; i++)
         swap(str[i], str[n - i - 1]);
 }
+//making the constructors of the class
+
+BigDecimalInt::BigDecimalInt()
+{
+    num="";
+}
+
+
+BigDecimalInt::BigDecimalInt(string decstr)
+{
+    num=decstr;
+}
+
+BigDecimalInt :: BigDecimalInt (int decint)
+{
+    if (decint < 0)
+    {
+        decint*=-1;
+
+        while ( decint!=0)
+    {
+
+        num+=(decint%10)+'0';
+        decint-= decint%10;
+        decint/=10;
+
+    }
+    num+='-';
+    }
+
+
+
+ else   if (decint == 0)
+    {
+        //cout<<"i'm here\n";
+        num="0";
+    }
+
+
+else if (decint >0 ){
+    while ( decint!=0)
+    {
+
+        num+=(decint%10)+'0';
+        decint-= decint%10;
+        decint/=10;
+
+    }
+}
+    reverseStr(num);
+
+}
+
 
 
