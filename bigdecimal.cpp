22b4ee66a3a859039ba5e8d669bea5e0f3616033
ahmedyasserbastawessy <101553898+ahmedyasserbastawessy@users.noreply.github.com>
2022-10-12 23:14:05 +0200

    Update bigdecimal.cpp

--- bigdecimal.cpp
+++ bigdecimal.cpp
@@ -1,2 +1,14 @@
+#include "bigdecimalint.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+//making a function to reverse string
+void reverseStr(string& str)
+{
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+}
 
 
