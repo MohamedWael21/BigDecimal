e486b278cf075b68dd36d0bfdb90a402f41b6415
programmerkid <mohame171819@gmail.com>
2022-10-25 14:51:50 +0200

    add test cases

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include "bigdecimalint.h"
 
 int main(){
-    BigDecimalInt a("123"), b("23");
-    cout << a + b << '\n';
+    BigDecimalInt a("1233939028390390283098093802803982938093492804938098090");
+    BigDecimalInt b("-2338023093289083094809393080980948039800803804982080382");
     
+    cout << a+b << '\n';
+    
+    cout << a-b << '\n';
+
+    cout << (a>b?"Yes":"No") << '\n';
+
+    cout << (a<b?"Yes":"No") << '\n';
+
+    cout << (a==b?"Yes":"No") << '\n';
+
+    cout << a.sign() << '\n';
+    
+    cout << b.sign() << '\n';
+
+    cout << a.size() << '\n';
+    cout << b.size() << '\n';
+
+    a = b;
+
+    cout << a << '\n';
+
 }
